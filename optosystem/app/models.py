6a0e72d008d968016cc2ab8c512dff22903d4e32
brettbust <bsbustamantez@gmail.com>
2020-11-16 18:29:50 -0300

    creacion de base de datos y migraciones de los modelos

--- optosystem/app/models.py
+++ optosystem/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import PROTECT, RESTRICT
 
 # Create your models here.
 
@@ -12,6 +13,7 @@ def __str__(self):
 
    
 class pedidos(models.Model):
+    relacion_con_productos = models.ForeignKey(productos, on_delete=PROTECT) 
     estado_del_pedido = models.CharField(max_length= 20, verbose_name= 'estado del pedido')
     tipo_de_pago = models.CharField(max_length= 20, verbose_name= 'tipo de pago')
 
@@ -20,23 +22,26 @@ def __str__(self):
     return self.estado_del_pedido, self.tipo_de_pago
 
 
-class paciente(models.Model):
-  nombre: models.CharField(max_length= 20, verbose_name= 'nombre del paciente')
-  apellido: models.CharField(max_length= 20, verbose_name= 'apellido del paciente')
-  edad: models.IntegerField(max_length= 3, verbose_name= 'edad del paciente')
-  peso: models.FloatField(max_length= 3, verbose_name= 'peso')
-  altura: models.FloatField(max_length= 3, verbose_name= 'altura')
+class pacientes(models.Model):
+  relacion_con_pedidos= models.ForeignKey(pedidos, on_delete=PROTECT) 
+  nombre= models.CharField(max_length= 20, verbose_name= 'nombre del paciente')
+  apellido= models.CharField(max_length= 20, verbose_name= 'apellido del paciente')
+  edad= models.IntegerField(max_length= 3, verbose_name= 'edad del paciente')
+  peso= models.FloatField(max_length= 3, verbose_name= 'peso')
+  altura= models.FloatField(max_length= 3, verbose_name= 'altura')
 
 def __str__(self):
     return self.nombre, self.apellido, self.edad, self.peso, self.altura
    
-class turno(models.Model):
+class turnos(models.Model):
+    relacion_con_pacientes= models.ForeignKey(pacientes, on_delete=PROTECT) 
     fecha_de_turno = models.DateField(verbose_name= 'fecha de turno')
     medico_asignado =  models.CharField(max_length= 20, verbose_name= 'medico asignado')
 
 def __str__(self):
     return self.fecha_de_turno, self.medico_asignado
 class historial(models.Model):
+    relacion_con_pacientes= models.ForeignKey(pacientes, on_delete=PROTECT) 
     historial_medico = models.CharField(max_length= 200, verbose_name= 'historial medico')
     
     def __str__(self):
