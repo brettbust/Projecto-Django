e460f5bb960f1ac6453bf867eea0c5da80075af8
brettbust <bsbustamantez@gmail.com>
2020-11-16 12:14:49 -0300

    include de urls del proyecto agregado

--- optosystem/app/views.py
+++ optosystem/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-#
+
 
 def inicio(request):
     
--- optosystem/optosystem/urls.py
+++ optosystem/optosystem/urls.py
@@ -15,18 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-#importando vistas
-from app import views
-
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.inicio, name ="Inicio"),
-    path('secretaria',views.secretaria, name ="Secretaria"),
-    path('profesional_Medico',views.profesional_Medico, name ="Profesional_Médico"),
-    path('ventas',views.ventas, name ="Ventas"),
-    path('taller',views.taller, name ="Taller"),
-    path('gerencia',views.gerencia, name ="Gerencia"),
-
+    path('', include('app.urls')), 
 ]
